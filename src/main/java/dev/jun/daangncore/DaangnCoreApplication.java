f840072f2f13f75872c7344be56d8c3d2837bb60
JunHyun <junhyun1001@gmail.com>
2025-12-10 11:27:48 +0900

    chore: Oracle DB 및 MyBatis 설정

---
+++ src/main/java/dev/jun/daangncore/DaangnCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.jun.daangncore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaangnCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jun/daangncore/DaangnCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jun.daangncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

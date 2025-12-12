395583f87ebeb27d157ef1d22c1a77c94412b38c
JunHyun <junhyun1001@gmail.com>
2025-12-13 03:26:02 +0900

    chore: 의존성 추가

---
+++ src/main/java/dev/jun/daangncore/entity/dto/request/SignupDto.java
@@ -0,0 +1,27 @@
+package dev.jun.daangncore.entity.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{8,20}$")
+    private String password;
+
+    @NotBlank
+    @Size(min = 2, max = 8)
+    private String nickname;
+
+
+}
